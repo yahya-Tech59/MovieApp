5f3bdb2cece2a9418e52bd255d78f17ee8a944c8
yahya-cade <yahyekade790@gmail.com>
2023-09-05 12:02:17 +0300

    first upload of movieapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { MovieList } from "./components/MovieList";
+import { MovieListHeading } from "./components/MovieListHeading";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-item-center mt-4 mb-4">
+        <MovieListHeading />
+      </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
---
+++ src/components/MovieListHeading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MovieListHeading = (props) => {
+  return (
+    <div className="col">
+      <h1>{props.heading}</h1>
+    </div>
+  );
+};
