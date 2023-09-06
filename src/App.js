b1c581217ea6ef7004a0826746e25439bf00bed6
yahya-cade <yahyekade790@gmail.com>
2023-09-06 11:03:37 +0300

    add to favourites added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { MovieList } from "./components/MovieList";
 import { MovieListHeading } from "./components/MovieListHeading";
+import { SearchBox } from "./components/SearchBox";
+import { AddFavourites } from "./components/AddFavourites";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=e3e1f78d`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-item-center mt-4 mb-4">
-        <MovieListHeading />
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
         <MovieList movies={movies} />
---
+++ src/components/AddFavourites.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BsFillHeartFill } from "react-icons/bs";
+
+export const AddFavourites = () => {
+  return (
+    <div>
+      <span className="mr-2">Add To Favourites</span>
+      <BsFillHeartFill />
+    </div>
+  );
+};
